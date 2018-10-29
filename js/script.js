abf16df41a9a41cfe091dd0da200e2c996f8dbeb
Jared Le Doux <jaredledoux@Jareds-MacBook-Pro.local>
2018-10-29 15:44:24 -0700

    Created question objects with arrays to keep track of selected answer choices.  Added to handleAnswerSelect to allow for data handling.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 
 
-//global answer choice var
+//global answer choice var - for attaching event listeners
 var globalAnswerChoices;
 
+//global active question variables
+var activeQuestion;
+var selectedAnswer;
+var activeQuestionChoices;
+
+
+function Question() {
+
+  this.choices = [0,0,0,0,0,0];
+
+}
+
+var question1 = new Question();
+var question2 = new Question();
+var question3 = new Question();
+var question4 = new Question();
+var question5 = new Question();
+var question6 = new Question();
+var question7 = new Question();
 
 //higlights selected answer green when user clicks on it.
 function handleAnswerSelect(event) {
-    var selectedAnswer = event.target;
+    selectedAnswer = event.target;
 
     //stores full list element (ul) for active question
-    var activeQuestion = selectedAnswer.parentNode;
+    activeQuestion = selectedAnswer.parentNode;
     //stores nodelist of active question
-    var activeQuestionChoices = activeQuestion.children;
+    activeQuestionChoices = activeQuestion.children;
     console.log(activeQuestionChoices);
     //checks for and deselects (if found) previously selected answer choice within question clicked.
 
@@ -19,28 +38,93 @@ function handleAnswerSelect(event) {
 
           if (activeQuestionChoices[i].className = "answer-item-selected") {
 
-              var previousSelection = activeQuestionChoices[i];
-              previousSelection.setAttribute("class", "answer-item");
-
-            }
+            var previousSelection = activeQuestionChoices[i];
+            previousSelection.setAttribute("class", "answer-item");
           }
-
+        }
       selectedAnswer.setAttribute("class", "answer-item-selected");
+
+//zeros question status array in all objects
+
+    for (var i = 0; i < 6; i++) {
+
+      activeQuestion.id
+
+
 
     }
 
-  // for (var i = 0; i < activeQuestionChoices.length; i++) {
-  //
-  //       // if (i === selectedAnswer.id)
-  //       //   {continue;}
-  //
-  //           if (activeQuestionChoices[i].className = "answer-item-selected") {
-  //
-  //             var previousSelection = activeQuestionChoices[i];
-  //             previousSelection.setAttribute("class", "answer-item");
-  //
-  //         }
-      //  }
+    switch (activeQuestion.id) {
+
+      case "c1":
+
+        question1.choices = [0,0,0,0,0,0];
+
+            if (question1.choices[selectedAnswer.id] === 0) {
+              question1.choices[selectedAnswer.id]++;
+            }
+      break;
+
+      case "c2":
+
+        question2.choices = [0,0,0,0,0,0];
+
+          if (question2.choices[selectedAnswer.id] === 0) {
+            question2.choices[selectedAnswer.id]++;
+            }
+      break;
+
+      case "c3":
+
+        question3.choices = [0,0,0,0,0,0];
+
+          if (question3.choices[selectedAnswer.id] === 0) {
+            question3.choices[selectedAnswer.id]++;
+            }
+      break;
+
+      case "c4":
+
+        question4.choices = [0,0,0,0,0,0];
+
+          if (question4.choices[selectedAnswer.id] === 0) {
+          question4.choices[selectedAnswer.id]++;
+            }
+      break;
+
+      case "c5":
+
+        question5.choices = [0,0,0,0,0,0];
+
+          if (question5.choices[selectedAnswer.id] === 0) {
+          question5.choices[selectedAnswer.id]++;
+            }
+      break;
+
+      case "c6":
+
+        question6.choices = [0,0,0,0,0,0];
+
+          if (question6.choices[selectedAnswer.id] === 0) {
+          question6.choices[selectedAnswer.id]++;
+            }
+      break;
+
+      case "c7":
+
+        question6.choices = [0,0,0,0,0,0];
+
+          if (question7.choices[selectedAnswer.id] === 0) {
+          question7.choices[selectedAnswer.id]++;
+            }
+      break;
+
+    }
+}
+  // console.log(activeQuestion);
+
+
+
 
 //Onload, gets and attaches a listener to each answer choice element on page.
 
@@ -53,9 +137,9 @@ function onLoad() {
 
     for (var i = 0; i < el.length; i++) {
         el[i].addEventListener('click', handleAnswerSelect);
-
     }
   }
   //console.log(globalAnswerChoices)
 }
+
 window.addEventListener('load', onLoad)
